1d7fcd1bce42ebf98d715483e64633f1b00a1d79
RoshniMiranda <56973029+RoshniMiranda@users.noreply.github.com>
2020-05-31 11:01:21 +0530

    Create Prob1.java

---
+++ Prob1.java
@@ -0,0 +1,25 @@
+/*public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       
+      int A;
+      int B;
+      int C;
+      Scanner s = new Scanner(System.in);
+      System.out.println(" ");
+      A = s.nextInt();
+      System.out.println(" ");
+      B = s.nextInt();
+      
+      C = A+B;
+      System.out.println(" "+C);
+      s.close();
+    }
+}
+   
+      
